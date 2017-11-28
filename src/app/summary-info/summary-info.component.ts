0744017e848d0c36c467e7c4308fb89ecf262d77
Lakmal Caldera <lakmal.developer@gmail.com>
2017-11-28 21:00:31 +0100

    updated admission cards and allowed download them

--- src/app/summary-info/summary-info.component.ts
+++ src/app/summary-info/summary-info.component.ts
@@ -119,7 +119,7 @@ export class SummaryInfoComponent implements OnInit {
         this.formService.loadUserData(res)
         this.formService.setStudentHash(data.hash);
         if (!this.paymentService.isStudentPaymentComplete()) {
-          this.onPayment();
+          //this.onPayment();
         } else {
           this.loadDataIntoSummary()
         }
@@ -158,15 +158,15 @@ export class SummaryInfoComponent implements OnInit {
   }
 
   downloadMyAdmission() {
-    // this.formService.downloadAdmissionCard().subscribe((res: any) => {
-    //   if (res) {
-    //     console.log("PDF downloaded");
-    //     let filename = `admission_card_${this.formService.generalInfo.nic}_${this.formService.generalInfo.firstProgram}.pdf`;
-    //     FileSaver.saveAs(res, filename);
-    //   }
-    // }, (res: Response) => {
-    //   this.router.navigate(['../request-error'], { relativeTo: this.route });
-    // });
+    this.formService.downloadAdmissionCard().subscribe((res: any) => {
+      if (res) {
+        console.log("PDF downloaded");
+        let filename = `admission_card_${this.formService.generalInfo.nic}_${this.formService.generalInfo.firstProgram}.pdf`;
+        FileSaver.saveAs(res, filename);
+      }
+    }, (res: Response) => {
+      this.router.navigate(['../request-error'], { relativeTo: this.route });
+    });
   }
 }
 
